3845ed7be5e7aac94067d265b528ad7ff9cab801
lrod <rleslie1015@gmail.com>
2019-03-19 15:59:14 -0700

    started building carousel component

---
+++ carousel.js
@@ -0,0 +1,34 @@
+class Slide {
+    constructor(carousel){
+        this.carousel = carousel;
+        this.slideNumber = 0;
+        this.carouselImages = (document.querySelectorAll(`.carouselImg[data-slide]`));
+        console.log(this.carouselImages);
+        this.currentSlide = this.carousel.querySelectorAll(`.slide[data-slide]`);
+
+        this.previousBtn = carousel.querySelector(".previous");
+        this.nextBtn = carousel.querySelector(".next");
+        this.previousBtn.addEventListener("click", () => this.previousSlide());
+        this.nextBtn.addEventListener("click", () => this.nextSlide());
+    }
+        nextSlide = () => {
+        this.carouselImages[this.slideNumber].style.display = "none";
+        this.slideNumber++;
+        if (this.slideNumber > 3){
+            this.slideNumber = 0;
+        }
+        this.carouselImages[this.slideNumber].style.display = "block";
+}
+        previousSlide = () => {
+       
+        this.slideNumber--;
+}
+}
+
+const slides = document.querySelectorAll(".carousel");
+
+console.log(slides);
+
+slides.forEach(function(slide){
+    return new Slide(slide);
+});
